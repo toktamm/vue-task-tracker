b2b076c4b32c431ab352b1f802c20182dbc8c214
Toktam <ttmohebbi@gmail.com>
2021-07-29 19:21:57 -0400

    build the task component and add styling to it

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,5 +1,8 @@
 <template>
-  <h3>{{ task.text }}</h3>
+  <div class="task">
+    <h3>{{ task.text }}</h3>
+    <p>{{ task.day }}</p>
+  </div>
 </template>
 
 <script>
@@ -10,3 +13,22 @@ export default {
   },
 };
 </script>
+
+<style>
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+.task.reminder {
+  border-left: 5px solid green;
+}
+
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
