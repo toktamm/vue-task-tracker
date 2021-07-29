8abbe8745678fa651ff03677de621ffcd0c1a2ba
Toktam <ttmohebbi@gmail.com>
2021-07-29 18:10:49 -0400

    iterate over tasks

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,12 +1,14 @@
 <template>
-  
+  <div v-for="task in tasks">
+    <h3>{{ task.text }}</h3>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Tasks',
+  name: "Tasks",
   props: {
-    tasks: Array
-  }
-}
+    tasks: Array,
+  },
+};
 </script>
