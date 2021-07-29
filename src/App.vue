1a05a07c43b63b0a66625c5491441d2607c715d6
Toktam <ttmohebbi@gmail.com>
2021-07-29 17:30:39 -0400

    add created lifecycle method and add the tasks to it

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,31 @@ export default {
   },
   data() {
     return {
-      tasks: []
-    }
-  }
+      tasks: [],
+    };
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctors Appointment",
+        day: "March 5th at 2:30pm",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Meeting with boss",
+        day: "March 6th at 1:30pm",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "Food shopping",
+        day: "March 7th at 2:00pm",
+        reminder: false,
+      },
+    ];
+  },
 };
 </script>
 
