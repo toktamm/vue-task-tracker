bbfd18cfe20717bddabb96996a060e651a0f4503
Toktam <ttmohebbi@gmail.com>
2021-07-29 17:09:00 -0400

    create the onClick method

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,7 @@
 <template>
-  <button @click="onClick()" :style="{ background: color }" class="btn">{{ text }}</button>
+  <button @click="onClick()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
 </template>
 
 <script>
@@ -8,6 +10,11 @@ export default {
   props: {
     text: String,
     color: String,
+  },
+  methods: {
+    onClick() {
+      console.log("button clicked");
+    },
   },
 };
 </script>
